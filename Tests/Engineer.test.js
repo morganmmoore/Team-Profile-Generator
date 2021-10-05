a46f020bb5e4d6d91350e0f6a5c1eb991cde6fab
morganmmoore <morganmmoore0@gmail.com>
2021-10-04 20:25:21 -0400

    completed Engineer.test

---
+++ Tests/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("can set GitHub", () => {
+    const testGithub = githubUser;
+    const e = new Engineer("Fred", 22, "Fred@gmail.com", testGithub);
+    expect(e.github).toBe(testGithub);
+});
+
+test("Can get gitHub username via getGithub()", () => {
+    const testGithub = githubUser;
+    const e = new Engineer("Sally", 4, "Sally@yahoo.com", testGithub);
+    expect(e.getGithub()).toBe(testGithub);
+});
+
+test("getRole() should return 'Engineer'", () => {
+    const role = "Engineer";
+    const e = new Engineer("Mandy", 17, "Mandy@gmail.com", role);
+    expect(e.getRole()).toBe(role);
+});
