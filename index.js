1f6b3b915c68deb88fe0ff56c11e3ec3b73e0960
morganmmoore <morganmmoore0@gmail.com>
2021-10-05 17:30:02 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 
 const allEmployees = [];
 
-function createManager() {
+init = () => {
+    startHtml();
+    mainPrompt();
+}
+
+createManager = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -31,11 +37,12 @@ function createManager() {
         console.log(data);
         const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
         allEmployees.push(manager);
+        generateHtml(allEmployees);
         mainPrompt();
     })
 }
 
-function createIntern() {
+createIntern = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -61,11 +68,12 @@ function createIntern() {
         console.log(data);
         const intern = new Intern(data.name, data.id, data.email, data.school);
         allEmployees.push(intern);
+        generateHtml(allEmployees);
         mainPrompt();
     })
 }
 
-function createEngineer() {
+createEngineer = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -91,11 +99,12 @@ function createEngineer() {
         console.log(data);
         const engineer = new Engineer(data.name, data.id, data.email, data.github);
         allEmployees.push(Engineer);
+        generateHtml(allEmployees);
         mainPrompt();
     })
 }
 
-function mainPrompt() {
+mainPrompt = () => {
     inquirer.prompt([
         {
             type: 'list',
@@ -111,12 +120,113 @@ function mainPrompt() {
         } else if (data.main === 'Add Manager') {
             createManager();
         } else if (data.main === 'Done') {
-            generateTeam();
+            generateHtml();
+            completeHtml();
         }
     })
 }
 
-mainPrompt();
-createManager();
-createEngineer();
-createIntern();
+startHtml = () => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+     <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+         <title>Team Profile</title>
+     </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand w-100 text-center h1">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row col-6">`;
+    fs.writeFile('./output/team.html', html, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Team successfully created!');
+        }
+    })
+}
+
+generateHtml = (employee) => {
+    return new Promise(function(resolve, reject) {
+        const name = employee.getName();
+        const role = employee.getRole();
+        const id = employee.getId();
+        const email = employee.getEmail();
+
+        let data = '';
+
+        if (role === 'Manager') {
+            const github = employee.getOfficeNumber();
+            
+            data = `<div class="card col-6" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">Office Number: ${officeNumber}</li>
+            </ul>
+        </div>`
+        } else if (role === 'Engineer') {
+            const github = employee.getGithub();
+
+            data = `<div class="card col-6" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">Office Number: ${github}</li>
+            </ul>
+        </div>`
+        } else if (role === 'Intern') {
+            const school = employee.getSchool();
+
+            data = `<div class="card col-6" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">Office Number: ${school}</li>
+            </ul>
+        </div>`
+        }
+        fs.appendFile('./output/team.html', data, (err) => {
+            if (err) {
+                return reject(err);
+            } else {
+                return resolve();
+            }
+        })
+    })
+}
+
+completeHtml = () => {
+    const html =   `            </div>
+    </div>
+</body>
+</html>`;
+
+fs.appendFile('./output/team.html', data, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Complete!');
+    }
+})
+}
+
+init();
